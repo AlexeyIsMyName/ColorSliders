ae92e456b420b714a2d9e28df2e3429a3b24b3ba
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-11 17:11:44 +0300

    Made Logic

--- ColorSliders/ViewController.swift
+++ ColorSliders/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        updateUI()
     }
-
-
+    
+    @IBAction func sliderChanged() {
+        updateUI()
+    }
+    
+    private func updateUI() {
+        let redColor = CGFloat(redSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+        
+        redLabel.text = String(format: "%.2f", redColor)
+        greenLabel.text = String(format: "%.2f", greenColor)
+        blueLabel.text = String(format: "%.2f", blueColor)
+        
+        colorView.backgroundColor = UIColor(red: redColor,
+                                            green: greenColor,
+                                            blue: blueColor,
+                                            alpha: 1.0)
+    }
 }
-
